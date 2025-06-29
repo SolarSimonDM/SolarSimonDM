433acf2427db3d875549d40de6a5d6df6873b0cb
geody2000 <geody2000@gmail.com>
2025-06-29 22:17:30 +0300

    skip solarloader url param

--- js/solarloader.js
+++ js/solarloader.js
@@ -1,27 +1,31 @@
+const url_params = new URLSearchParams(window.location.search);
+const skip_solarloader = url_params.get("ss") === "1";
+
 const solarloader_key = "solarvisitor_time_of_seeing_solarloader";
 const now = Date.now();
 const solartime_to_pass_before_showing_solarloader_again = 7 * 24 * 60 * 60 * 1000;
 const solarvisitor_time_of_seeing_solarloader = localStorage.getItem(solarloader_key);
 
-// Only skip loader if time has passed and user previously clicked Enter button from the terminal
-if (solarvisitor_time_of_seeing_solarloader &&
-    now - parseInt(solarvisitor_time_of_seeing_solarloader) < solartime_to_pass_before_showing_solarloader_again) {
-    
+// If url has ss=1, skip solarloader
+if (skip_solarloader) {
+    document.getElementById("solarloader").style.display = "none";
+    document.getElementById("solarhome").style.display = "block";
+    document.body.className = "solarbody_homepage";
+} else if (
+    solarvisitor_time_of_seeing_solarloader &&
+    now - parseInt(solarvisitor_time_of_seeing_solarloader) < solartime_to_pass_before_showing_solarloader_again
+) {
     document.getElementById("solarloader").style.display = "none";
     document.getElementById("solarhome").style.display = "block";
     document.body.className = "solarbody_homepage";
 } else {
-    // Don't store timestamp yet — only show solarloader
     document.getElementById("solarhome").style.display = "none";
     document.body.className = "solarloader";
-    document.getElementById("ENTER_SOLAR").style.display = "block";    
+    document.getElementById("ENTER_SOLAR").style.display = "block";
 }
 
-// Called when user actually clicks Enter button from terminal
 function solarterminal_button_enter() {
-    // Now save the time
     localStorage.setItem(solarloader_key, now.toString());
-
     document.getElementById("solarloader").style.display = "none";
     document.getElementById("solarhome").style.display = "block";
     document.body.className = "solarbody_homepage";
